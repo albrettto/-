b18a98ee2398e3458a3c7dd777381467ff45f783
khusamov Albert <khusamov01@mail.ru>
2020-10-23 04:04:53 +0500

    Class_And_Objects v1.4
    Реализация вывода в консоль. Помещение дочерного класса в класс-предок.

--- Объекты и классы.cpp
+++ Объекты и классы.cpp
@@ -70,9 +70,10 @@ public:
     }
 };
 
-class ColoredLine : Line {
-public:
+class ColoredLine : public Line {
+private:
     string color;
+public:
     ColoredLine(): Line() {
         color = "черный";
         printf("Конструктор ColoredLine(): Line(). color = %s\n", color);
@@ -81,61 +82,88 @@ public:
         this->color = color;
         printf("Конструктор ColoredLine(string color, int x1, int y1, int x2, int y2) : Line(x1,y1,x2,y2). color = %s\n", color);
     }
-    ColoredLine(const ColoredLine & l_) : Line(l_) {
+    ColoredLine(const ColoredLine & l_){
         color = l_.color;
         p1 = new Point(*(l_.p1));
         p2 = new Point(*(l_.p2));
+        /*this->p1 = l_.p1;
+        this->p2 = l_.p2;*/
         printf("Конструктор ColoredLine(string color, int x1, int y1, int x2, int y2) : Line(x1,y1,x2,y2). color = %s)\n", color);
     }
     ~ColoredLine() {
-        printf("Деструктор ~ColoredLine()");
+        printf("Деструктор ~ColoredLine()\n");
+       /* delete p1;
+        delete p2;*/
     }
 };
+
+void show(int i){ printf("\n%i)\n", i); }
 
 int main()
 {
     int i = 1;
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    /*SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);*/
+    setlocale(LC_ALL, "Russian"); // задаём русский текст
+    //system("chcp 1251"); // настраиваем кодировку консоли
+
+    printf("\t\tДемонстрация класса Point:\n");
     {
         int x = 1, y = 2;
-        printf("\n%i)\n", i++);
+        show(i++);
         Point p1;
-        printf("\n%i)\n", i++);
+        show(i++);
         Point p2(x, y);
-        printf("\n%i)\n", i++);
+        show(i++);
         Point p3(p2);
-        printf("\n%i)\n", i++);
+        show(i++);
         p3.reset();
-        printf("\n%i)\n", i++);
+        show(i++);
         p3.move(5, 6);
-        printf("\n%i)\n", i++);
+        show(i++);
     }
+    printf("\n\t\tДемонстрация класса Line:\n");
     {
         int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
-        printf("\n%i)\n", i++);
+        show(i++);
         Line* l = new Line;
-        printf("\n%i)\n", i++);
+        show(i++);
         Line *l1 = new Line(x1, y1, x2, y2);
-        printf("\n%i)\n", i++);
+        show(i++);
         Line *l2 = new Line(*l1);
+        show(i++);
         l1->get_length(*l1);
-        printf("\n%i)\n", i++);
+        show(i++);
         delete l;
-        printf("\n%i)\n", i++);
+        show(i++);
         delete l1;
-        printf("\n%i)\n", i++);
+        show(i++);
         delete l2;
     }
+    printf("\n\t\tДемонстрация класса ColoredLine:\n");
     {
-        printf("\n%i)\n", i++);
-        ColoredLine l_1;
-        printf("\n%i)\n", i++);
-        ColoredLine l_2("красный", 1, 2, 3, 4);
-        printf("\n%i)\n", i++);
-        ColoredLine l_3(l_2);
+        int x1 = 5, y1 = 6, x2 = 6, y2 = 5;
+        string color = "красный";
+        show(i++);
+        ColoredLine* l_1 = new ColoredLine;
+        show(i++);
+        ColoredLine *l_2 = new ColoredLine(color, x1, y1, x2, y2);
+        show(i++);
+        ColoredLine *l_3 = new ColoredLine(*l_2);
+        show(i++);
+        delete l_1;
+        show(i++);
+        delete l_2;
+        show(i++);
+        delete l_3;
     }
-
-
+    printf("\n\t\tДемонстрация помещения класса ColoredLine в класс-предок :\n");
+    {
+        show(i++);
+        Line* l = new ColoredLine("голубой", 1, 2, 3, 4); 
+        show(i++);
+        delete l;
+    }
+    system("pause");
     return 0;
 }
